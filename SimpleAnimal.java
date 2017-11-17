daf7fb4658114f9f1ff49894e2691186cb7aaca3
bishasaurus <bishasaurus@gmail.com>
2017-11-17 18:57:57 +0200

    Create SimpleAnimal.java

---
+++ SimpleAnimal.java
@@ -0,0 +1,15 @@
+package AnimalDex;
+public class SimpleAnimal {
+	
+	String animalName;
+	String animalSpecies;
+	String animalColor;
+	
+	SimpleAnimal (String aAnimalName, String aAnimalSpecies, String aAnimalColor)
+	{
+		this.animalName = aAnimalName;
+		this.animalSpecies = aAnimalSpecies;
+		this.animalColor = aAnimalColor;
+	}
+
+}
