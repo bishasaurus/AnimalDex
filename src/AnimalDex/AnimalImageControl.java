426e01bb00063dcffb5479e8441f5c54ff3a91ec
bishasaurus <bishasaurus@gmail.com>
2017-12-08 20:44:32 +0200

    Changed primary image to be used

--- src/AnimalDex/AnimalImageControl.java
+++ src/AnimalDex/AnimalImageControl.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+
 public class AnimalImageControl { //class responsible for AnimalDex's image
 
 	static String imageControl() //tries to load the default AnimalDex image and checks its availability via ImageIO and IOException; if ImageIO fails to load the image, IOException triggers and loads an alternative image from different source
@@ -12,7 +13,7 @@ public class AnimalImageControl { //class responsible for AnimalDex's image
 	String primaryImage = null;
 	
 		try { //tries to find the default image
-			Image handleImage = ImageIO.read(new File("src/image/animaliaT.png"));//handleImage is not used itself, only for checking if the searched image is available
+			Image handleImage = ImageIO.read(new File("src/image/animalia.jpg"));//handleImage is not used itself, only for checking if the searched image is available
 
 			primaryImage = "src/image/animaliaT.png";
 			return primaryImage;		
@@ -21,6 +22,9 @@ public class AnimalImageControl { //class responsible for AnimalDex's image
 			
 				primaryImage = "src/AnimalDex/animals.jpg";
 				return primaryImage;	
+			
 		}
+		
 	}
+
 }
